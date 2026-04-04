5354b3f40b48641c9bf05ae66c5b8ab35df74f23
Juanma El Cano <juanmaelcano96@gmail.com>
2026-04-04 21:12:55 +0200

    update content and layout

--- components/MenuCategory.tsx
+++ components/MenuCategory.tsx
@@ -1,14 +1,36 @@
 import MenuItem from "./MenuItem";
 
-export default function MenuCategory({ category, items }: { category: string; items: { name: string; price: string }[] }) {
+type Item = {
+  name: string;
+  description: string;
+  priceP: string;
+  priceG?: string;
+};
+
+type Props = {
+  category: string;
+  items: Item[];
+};
+
+export default function MenuCategory({ category, items }: Props) {
   return (
-    <div style={{ marginBottom: "24px" }}>
-      <h2 style={{ fontSize: "20px", fontWeight: "bold", marginBottom: "8px" }}>{category}</h2>
-      <div style={{ border: "1px solid #ccc", borderRadius: "8px", padding: "8px" }}>
-        {items.map((item) => (
-          <MenuItem key={item.name} name={item.name} price={item.price} />
-        ))}
+    <div style={{ border: "1px solid #ccc", borderRadius: "8px", padding: "16px" }}>
+  
+      {/* Cabecera precios */}
+      <div style={{ display: "flex", justifyContent: "space-between", padding: "4px 0", fontWeight: "bold", borderBottom: "1px solid #ccc" }}>
+        <span>{category}</span>
+        <span>P / G</span>
       </div>
+
+      {items.map((item) => (
+        <MenuItem
+          key={item.name}
+          name={item.name}
+          description={item.description}
+          priceP={item.priceP}
+          priceG={item.priceG}
+        />
+      ))}
     </div>
   );
 }
--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -1,8 +1,22 @@
-export default function MenuItem({ name, price }: { name: string; price: string }) {
+type MenuItemProps = {
+  name: string;
+  description: string;
+  priceP: string;
+  priceG?: string;
+};
+
+export default function MenuItem({ name, description, priceP, priceG }: MenuItemProps) {
   return (
     <div style={{ display: "flex", justifyContent: "space-between", padding: "8px 0", borderBottom: "1px solid #ccc" }}>
-      <span>{name}</span>
-      <span>{price}</span>
+      <div>
+        <p>{name}</p>
+        <p style={{color: "grey"}}>{description}</p>
+      </div>
+      <span>
+        {priceP && priceG
+          ? `${priceP} / ${priceG}`
+          : priceP || priceG}
+      </span>
     </div>
   );
 }
